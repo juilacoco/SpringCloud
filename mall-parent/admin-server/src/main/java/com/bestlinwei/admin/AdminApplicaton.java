684374b3c91f24c90fed636b437860502cf28720
林慰 <wei.lin@flaginfo.com.cn>
2018-04-12 17:59:34 +0800

    框架搭建

--- mall-parent/admin-server/src/main/java/com/bestlinwei/admin/AdminApplicaton.java
+++ mall-parent/admin-server/src/main/java/com/bestlinwei/admin/AdminApplicaton.java
@@ -1,12 +1,76 @@
 package com.bestlinwei.admin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableAdminServer
+@EnableAdminServer
 public class AdminApplicaton {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplicaton.class, args);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
