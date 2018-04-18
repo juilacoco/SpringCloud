7ae7678434ef93f5e1bc382ba5ca9a91585bbbee
林慰 <wei.lin@flaginfo.com.cn>
2018-04-18 18:40:20 +0800

    框架构建

--- mall-parent/admin-server/src/main/java/com/bestlinwei/admin/AdminApplicaton.java
+++ mall-parent/admin-server/src/main/java/com/bestlinwei/admin/AdminApplicaton.java
@@ -3,8 +3,10 @@ package com.bestlinwei.admin;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class,DataSourceAutoConfiguration.class})
 @EnableAdminServer
 public class AdminApplicaton {
     public static void main(String[] args) {
--- mall-parent/eureka-server/src/main/java/com/bestlinwei/eureka/EurekaApplication.java
+++ mall-parent/eureka-server/src/main/java/com/bestlinwei/eureka/EurekaApplication.java
@@ -2,9 +2,12 @@ package com.bestlinwei.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class,DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
